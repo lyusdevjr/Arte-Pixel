608ae0f4752f2519f3d9626ba8ca547e49ceb81e
Luis Henrique <lyusdevjr@gmail.com>
2023-05-23 16:27:13 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -42,7 +42,7 @@ window.addEventListener('load', () => {
             const viewerBoard = document.querySelector('#pixel-board');
 
             const generateBoard = () => {
-              for (let row = 0; row <= 5; row += 1) {
+              for (let row = 0; row < 5; row += 1) {
                 const rowDiv = document.createElement('div');
 
                 for (let index = 0; index < 5; index += 1) {
